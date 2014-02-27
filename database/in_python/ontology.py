18ea7e84826c49d813742333ca897fc3b652abab
Aleks Litynski <ajl5242@rit.edu>
2014-02-27 09:06:36 -0500

    The database now stores data "correctly". It has made me realize I need to confront "is a" and node re-use in general.
    The query system is written and totally un-tested. I'm fixing that now.
    
    Fixed a bug where I copied text from google docs into a comment. The ' in the google doc was not ascii and gave pypy a heart attack.

--- database/in_python/ontology.py
+++ database/in_python/ontology.py
@@ -3,136 +3,134 @@ import pickle
 import uuid
 
 class node:
-    def __init__(self, id, node_type, value):
-        self.id = id
-        self.type = node_type
-        self.value = value
+	def __init__(self, id, node_type, value):
+		self.id = id
+		self.type = node_type
+		self.value = value
 
 class edge:
-    def __init__(self, edge_type):
-        self.type = edge_type
-        self.weights = {}
+	def __init__(self, edge_type):
+		self.type = edge_type
+		self.weights = {}
 
-    #add support for ranges
-    #use: http://stackoverflow.com/questions/12141150/from-list-of-integers-get-number-closest-to-some-value
-    def add_weight(self, time, weight):
-        self.weights[time] = weight
+	#add support for ranges
+	#use: http://stackoverflow.com/questions/12141150/from-list-of-integers-get-number-closest-to-some-value
+	def add_weight(self, time, weight):
+		self.weights[time] = weight
 
 class ontology:
-    def __init__(self):
-        self.graph = nx.Graph()
-        pass
+	def __init__(self):
+		self.graph = nx.Graph()
+		pass
 
-    def add_node(self, type, value):
-        valid_nodes = [ "noun", 
-                        "relationship", 
-                        "value", 
-                        "type", 
-                        "verb", 
-                        "change", 
-                        "constraint"]
+	def add_node(self, type, value):
+		valid_nodes = [ "noun", 
+						"relationship", 
+						"value", 
+						"type", 
+						"verb", 
+						"change", 
+						"constraint"]
 
-        new_node = node( str(uuid.uuid1()), type, value )
+		new_node = node( str(uuid.uuid1()), type, value )
 
-        if type in valid_nodes:
-            self.graph.add_node( new_node )
-        else:
-            new_node = type + " is invalid node type"
-        return new_node
+		if type in valid_nodes:
+			self.graph.add_node( new_node )
+		else:
+			new_node = type + " is invalid node type"
+		return new_node
 
-    def add_edge(self, type, start, end, time, weight):
-        valid = False
-        st = start.type
-        et = end.type
-        if type == "knows_of"  and st == "noun": valid = True
-        #elif type == "is_a"      and st == "noun" and et == "noun": valid = True
-        
-        elif type == "describes" and st == "relationship": valid = True
-        elif type == "has_value" and st == "relationship" and et == "value": valid = True
-        elif type == "has_type"  and st == "relationship" and et == "type": valid = True
-        elif type == "regarding" and st == "relationship" and et == "noun": valid = True
-       
-        elif type == "is_constrained_by" and st == "relationship" and et == "constraint": valid = True
-        elif type == "leads_to"  and st == "verb" and et == "verb": valid = True
-        elif type == "causes"    and st == "verb" and end == "change": valid = True
-
-
-        #find existing edge else, add new edge
-        #add weight to edge
-        new_edge = "not a valid edge"
-        if valid:
-            is_new_edge = True
-            new_edge = edge(type)
-            for edge_off_start in self.graph.edges([start]): #itterating bad. Fix, fix, fix
-                if edge_off_start[0] == start and edge_off_start[1] == end:
-                    new_edge = self.get_edge_val(edge_off_start)
-                    is_new_edge = False
-                    break
-            if is_new_edge:
-                self.graph.add_edge(start, end, {"edge": new_edge})
-
-            new_edge.add_weight(time, weight)
-
-        return new_edge
+	def add_edge(self, type, start, end, time, weight):
+		valid = False
+		st = start.type
+		et = end.type
+		if type == "knows_of"  and st == "noun": valid = True
+		#elif type == "is_a"      and st == "noun" and et == "noun": valid = True
+		
+		elif type == "describes" and st == "relationship": valid = True
+		elif type == "has_value" and st == "relationship" and et == "value": valid = True
+		elif type == "has_type"  and st == "relationship" and et == "type": valid = True
+		elif type == "regarding" and st == "relationship" and et == "noun": valid = True
+	   
+		elif type == "is_constrained_by" and st == "relationship" and et == "constraint": valid = True
+		elif type == "leads_to"  and st == "verb" and et == "verb": valid = True
+		elif type == "causes"    and st == "verb" and end == "change": valid = True
 
 
+		#find existing edge else, add new edge
+		#add weight to edge
+		new_edge = "not a valid edge"
+		if valid:
+			is_new_edge = True
+			new_edge = edge(type)
+			for edge_off_start in self.graph.edges([start]): #itterating bad. Fix, fix, fix
+				if edge_off_start[0] == start and edge_off_start[1] == end:
+					new_edge = get_edge_val(edge_off_start, self.graph)
+					is_new_edge = False
+					break
+			if is_new_edge:
+				self.graph.add_edge(start, end, {"edge": new_edge})
 
-    #prints out the whole graph.
-    def print_all(self):
-        return self.show_locally()
+			new_edge.add_weight(time, weight)
 
-    #save database to disk
-    def save(self, file_name="database_dump.tdb"):
-        pickle.dump(self.graph, open(file_name, "w"))
-    #load db from disk
-    def load(self, file_name="database_dump.tdb"):
-        self.graph = pickle.load(open(file_name))
+		return new_edge
 
 
+
+	#prints out the whole graph.
+	def print_all(self):
+		return self.show_locally()
+
+	#save database to disk
+	def save(self, file_name="database_dump.tdb"):
+		pickle.dump(self.graph, open(file_name, "w"))
+	#load db from disk
+	def load(self, file_name="database_dump.tdb"):
+		self.graph = pickle.load(open(file_name))
 
 
 
 
-    def override_with_sample(self):
-        #createes an ontology and adds some fakie nodes.
-        self.graph = nx.Graph()
-        english = self.add_node("noun", "")
-        self.add_relationship(english, english, "named", "english")
-
-        "Chair, Table; Material: Plastic, wood, metal"
-        chair = self.new_noun_named("chair", english)
-        table = self.new_noun_named("table", english)
-        plastic = self.new_noun_named("plastic", english)
-        wood = self.new_noun_named("wood", english)
-        metal = self.new_noun_named("metal", english)
-        self.add_relationship(chair, plastic, "is_made_of", "True")
-        self.add_relationship(chair, wood, "is_made_of", "True")
-        self.add_relationship(chair, metal, "is_made_of", "True")
-        self.add_relationship(table, plastic, "is_made_of", "True")
-        self.add_relationship(table, wood, "is_made_of", "True")
-        self.add_relationship(table, metal, "is_made_of", "True")
 
 
+	def override_with_sample(self):
+		#createes an ontology and adds some fakie nodes.
+		self.graph = nx.Graph()
+		english = self.add_node("noun", "")
+		self.add_relationship(english, english, "named", "english")
 
-        bed = self.new_noun_named("bed", english)
-        bed_frame = self.new_noun_named("bed frame", english)
-        blanket = self.new_noun_named("blanket", english)
-        self.add_relationship(bed, bed_frame, "has_a", "True")
-        self.add_relationship(bed, blanket, "has_a", "True")
-
-        colors = self.new_nouns_named(["burgundy", "violet", "goldenrod",
-                        "fuchsia", "lavender", "beige", "azure",
-                        "chartreuse", "celadon", "sage", "paisley",
-                        "plaid", "tartan", "scarlet"], english)
-
-        for color in colors:
-            self.add_relationship(blanket, color, "colored", "True")
-
-        bed_sizes = self.new_nouns_named(["twin", "double", "queen", "king"], english)
-        for size in bed_sizes:
-            self.add_relationship(bed, size, "size", "True")
+		"Chair, Table; Material: Plastic, wood, metal"
+		chair = self.new_noun_named("chair", english)
+		table = self.new_noun_named("table", english)
+		plastic = self.new_noun_named("plastic", english)
+		wood = self.new_noun_named("wood", english)
+		metal = self.new_noun_named("metal", english)
+		self.add_relationship(chair, plastic, "is_made_of", "True")
+		self.add_relationship(chair, wood, "is_made_of", "True")
+		self.add_relationship(chair, metal, "is_made_of", "True")
+		self.add_relationship(table, plastic, "is_made_of", "True")
+		self.add_relationship(table, wood, "is_made_of", "True")
+		self.add_relationship(table, metal, "is_made_of", "True")
 
 
+
+		bed = self.new_noun_named("bed", english)
+		bed_frame = self.new_noun_named("bed frame", english)
+		blanket = self.new_noun_named("blanket", english)
+		self.add_relationship(bed, bed_frame, "has_a", "True")
+		self.add_relationship(bed, blanket, "has_a", "True")
+
+		colors = self.new_nouns_named(["burgundy", "violet", "goldenrod",
+						"fuchsia", "lavender", "beige", "azure",
+						"chartreuse", "celadon", "sage", "paisley",
+						"plaid", "tartan", "scarlet"], english)
+
+		for color in colors:
+			self.add_relationship(blanket, color, "colored", "True")
+
+		bed_sizes = self.new_nouns_named(["twin", "double", "queen", "king"], english)
+		for size in bed_sizes:
+			self.add_relationship(bed, size, "size", "True")
 
 
 
@@ -145,42 +143,44 @@ class ontology:
 
 
 
-    def show_locally(self):
-        for n in self.graph.nodes_iter():
-            print n.id + " " + n.type + " " + n.value
-
-        for e in self.graph.edges_iter():
-            print str(e[0].id) + " -" + str(self.get_edge_val(e).weights) + "-> " + str(e[1].id)
 
 
-    def new_nouns_named(self, names, lang):
-        return map( lambda x: self.new_noun_named(x, lang) , names)
-    def new_noun_named(self, name, lang):
-        new_node = self.add_node("noun", "")
-        self.add_relationship(new_node, lang, "named", "name")
-        return new_node
+	def show_locally(self):
+		for n in self.graph.nodes_iter():
+			print n.id + " " + n.type + " " + n.value
+
+		for e in self.graph.edges_iter():
+			print str(e[0].id) + " -" + str(get_edge_val(e).weights, self.graph) + "-> " + str(e[1].id)
 
 
-    def add_relationship(self, src, target, type, value, weight=100, time=1):
+	def new_nouns_named(self, names, lang):
+		return map( lambda x: self.new_noun_named(x, lang) , names)
+	def new_noun_named(self, name, lang):
+		new_node = self.add_node("noun", "")
+		self.add_relationship(new_node, lang, "named", "name")
+		return new_node
 
-        re = self.add_node("relationship", "")
-        ty = self.add_node("type", type)
-        va = self.add_node("value", value)
-        self.add_edge("has_type", re, ty, time, 100)
-        self.add_edge("has_value", re, va, time, 100)
-        self.add_edge("describes", re, src, time, weight)
-        self.add_edge("regarding", re, target, time, 100)
-        return re
 
-    def get_edge_val(self, edge_tuple):
-        return self.graph[edge_tuple[0]][edge_tuple[1]]["edge"]
+	def add_relationship(self, src, target, type, value, weight=100, time=1):
+
+		re = self.add_node("relationship", "")
+		ty = self.add_node("type", type)
+		va = self.add_node("value", value)
+		self.add_edge("has_type", re, ty, time, 100)
+		self.add_edge("has_value", re, va, time, 100)
+		self.add_edge("describes", re, src, time, weight)
+		self.add_edge("regarding", re, target, time, 100)
+		return re
+
+def get_edge_val(edge_tuple, graph):
+	return graph[edge_tuple[0]][edge_tuple[1]]["edge"]
 
 
 ont = ontology()
 ont.override_with_sample()
 #onr.save()
 #ont.load()
-ont.show_locally()
+#ont.show_locally()
 
 """
 
@@ -199,39 +199,6 @@ On/off
 Book
 is on: (bed, chair, table, floor)
 Title:
-(Dreams of Potatoes; Tequila Sunrise; The Kraken; 40 Cakes; Spectral Robot Task Force; The Vengeful Penguin; Ninja’s Guide to Ornamental Horticulture; Neko-nomicon; This is Not a Book)
-
-
-#prints perfectly formatted json. The X, Y, Size should be removed soon.
-def to_string(self):
-    return "{ " \
-    "\"id\": \"n" + str(self.id) + "\"," \
-    "\"label\": \"" + str(self.value) + "\"," \
-    "\"x\": " + str(random.uniform(0,10)) + "," \
-    "\"y\": " + str(random.uniform(0,10)) + "," \
-    "\"size\": " + str(3) + "},"
-
-
-def to_string(self):
-    return "{ " \
-    "\"id\": \"e" + str(self.id) + "\"," \
-    "\"source\": \"n" + str(self.start) + "\"," \
-    "\"target\": \"n" + str(self.end) + "\"},"
-
-
-#prints out the whole graph.
-def print_all(self):
-    printed = "{ \"nodes\":["
-    for node in self.nodes:
-        printed = printed + node.to_string()
-    printed = printed[:-1] + "], \"edges\": ["
-    for edge in self.edges:
-        printed = printed + "\n" + edge.to_string()
-    printed = printed[:-1] + "]}"
-    return printed
-
-
-
-
+(Dreams of Potatoes; Tequila Sunrise; The Kraken; 40 Cakes; Spectral Robot Task Force; The Vengeful Penguin; Ninjas Guide to Ornamental Horticulture; Neko-nomicon; This is Not a Book)
 
 """
--- database/in_python/query_fielder.py
+++ database/in_python/query_fielder.py
@@ -1,37 +1,107 @@
 import ontology
 import json
+import random
+
 class query_fielder:
 
 	def __init__(self):
 		pass
+
 	#just a stub. Eventually, it could parse a query and do better than a fat dump.
 	def field_query(self, query, ontology):
-		query = json.loads(query)
+
+		query_parsed = json.loads(query)
 		target_graph = ontology.graph
 
+
 		reply = "no data"
-		if query.type == "get":
-			reply = json.dumps(["reply", find_nodes(query, target_graph)])
+
+		if query_parsed["type"] == "get":
+			out = self.find_nodes(query_parsed["search"], target_graph)
+			out = self.convert_to_json(out)
+			reply = json.dumps(["reply", out])
 		else:
-			reply = "query type: " + query.type + " not implimented yet"
+			reply = "query type: " + query_parsed["type"] + " not implimented yet"
 		return reply
+
+
+	def convert_to_json(self, data_to_jsonify):
+		return str(data_to_jsonify)
+
+
+
 
 
 	def find_nodes(self, query, graph):
 		valid_nodes = []
-		for node in graph.nodes():  #itterate every node in the graph to find the one we seek. 
-									#Obviously, this has to get better.
-			(is_valid, valid_node) = is_valid_node(query, node)
+		for node in graph.nodes_iter():     #itterate every node in the graph to find the one we seek.
+											#Obviously, this has to get better.
+			(is_valid, valid_node) = self.is_valid_node(query, node, graph)
 			if is_valid:
 				valid_nodes.append(valid_node)
 
 		return valid_nodes
 
 
-	def is_valid_node(self, query, node):
-		#if node.id == query.search.id
+	def is_valid_node(self, query, node, graph):
+		all_good = True
+		if all_good:
+			try: all_good = query["id"] == node.id
+			except AttributeError: pass
+			except KeyError: pass
+		if all_good:
+			try:  all_good = query["type"] == node.type
+			except AttributeError: pass
+			except KeyError: pass
+		if all_good:
+			try: all_good = query["value"] == node.value
+			except AttributeError: pass
+			except KeyError: pass
+		"""
+		if all_good:
+			for edge in graph.edges_iter([node]):
 
-		return (False, "")
+			   edge_obj = ontology.get_edge_val(edge, graph)
+
+			   if all_good:
+				   try:  all_good = query["type"] == edge_obj.type
+				   except AttributeError: pass
+				   except KeyError: pass
+			   if all_good:
+				   try:
+					   time = query["weight-time"]
+					   all_good = query["weight-value"] == edge_obj.weights[time]
+				   except AttributeError: pass
+				   except KeyError: pass
+			   if all_good:
+				   try: all_good = (query["direction"] == "inbound" and node == edge[0])
+				   except AttributeError: pass
+				   except KeyError: pass
+			   if all_good:
+				   try:
+					   other_end = edge[0]
+					   if other_end == node: other_end = edge_obj[1]
+					   all_good = self.is_valid_node(query["terminal"], other_end, graph)
+				   except AttributeError: pass
+				   except KeyError: pass
+
+			   if not all_good: break
+		"""
+		#if node.id == query.search.id
+		return (True, node)
+
+
+
+qf = query_fielder()
+ont = ontology.ontology()
+ont.override_with_sample()
+
+test_query = '{"type": "get","search": {"edges": [{"direction": "inbound","type": "described_by","weight-time": "1","terminal": {"type": "adjective","edges": [{"terminal": {"type": "type","value": "name"}},{"terminal": {"type": "value","value": "fred"}}]}}]}}'
+
+print qf.field_query(test_query, ont)
+
+
+
 
 
 
@@ -59,5 +129,71 @@ class query_fielder:
 	}
 }
 
-"""
+{
+	"type": "get",
+	"search": {
+		"edges": [
+			{
+				"direction": "inbound",
+				"type": "described_by",
+				"weight-time": "1",
+				"terminal": {
+					"type": "adjective",
+					"edges": [
+						{
+							"terminal": {
+								"type": "type",
+								"value": "name"
+							}
+						},
+						{
+							"terminal": {
+								"type": "value",
+								"value": "fred"
+							}
+						}
+					]
+				}
+			}
+		]
+	}
+}
 
+
+
+
+
+#prints perfectly formatted json. The X, Y, Size should be removed soon.
+def to_string(self):
+	return "{ " \
+	"\"id\": \"n" + str(self.id) + "\"," \
+	"\"label\": \"" + str(self.value) + "\"," \
+	"\"x\": " + str(random.uniform(0,10)) + "," \
+	"\"y\": " + str(random.uniform(0,10)) + "," \
+	"\"size\": " + str(3) + "},"
+
+
+def to_string(self):
+	return "{ " \
+	"\"id\": \"e" + str(self.id) + "\"," \
+	"\"source\": \"n" + str(self.start) + "\"," \
+	"\"target\": \"n" + str(self.end) + "\"},"
+
+
+#prints out the whole graph.
+def print_all(self):
+	printed = "{ \"nodes\":["
+	for node in self.nodes:
+		printed = printed + node.to_string()
+	printed = printed[:-1] + "], \"edges\": ["
+	for edge in self.edges:
+		printed = printed + "\n" + edge.to_string()
+	printed = printed[:-1] + "]}"
+	return printed
+
+
+
+
+
+
+"""
