4fbf9474cb9f5330b2e5014b80fe34c88b25b5f0
Joe Prezioso <joeprezioso@comcast.net>
2014-03-04 10:38:04 -0500

    Player can sti on the floor and dance. Also, I fixed the weirdness with the chair.

--- Demo/Room_Demo.py
+++ Demo/Room_Demo.py
@@ -34,7 +34,7 @@ roomContents=[chair[0],table[0],bed[0]]
 #Since chair[0] and table[0] are the basic versions of these objects, we don't need to worry about the range not including them
 def inspectObject(obj):
 	if obj[0] == "chair":
-		roomContents[0] = chair[random.randint(0,3)] #pick a random chair
+		roomContents[0] = chair[random.randint(1,3)] #pick a random chair
 			
 	elif obj[0] == "table":
 		roomContents[1] = table[random.randint(1,3)]
@@ -94,86 +94,109 @@ def search(obj):
 				
 	# if ("sit on " + object[0]) in action:
 		# sitOnIt(obj[0])
-	
-
-		
 		
 	
 def chairNode(action):
 	if "chair" in action: 
 		
-			if "sit" in action:
-				sitOnIt(chair[0])
+		if "sit" in action:
+			sitOnIt(chair[0])
+		
+		elif "jump" in action:
+			jumpOnIt(chair[0])
 			
-			elif "jump" in action:
-				jumpOnIt(chair[0])
-				
-			elif "under" in action:
-				duckAndCover(chair[0])
-				
-			elif "lift" in action:
-				lift(chair[0])
+		elif "under" in action:
+			duckAndCover(chair[0])
 			
-			elif "search" in action:
-				search(chair[0])
+		elif "lift" in action:
+			lift(chair[0])
+		
+		elif "search" in action:
+			search(chair[0])
+		
+		elif(roomContents[0] == chair[0]):
+			inspectObject(chair)
+			print(roomContents[0])
 			
-			elif(roomContents[0] == chair[0]):
-				inspectObject(chair)
-				print(roomContents[0])
-				
-			else:
-				print(roomContents[0])
+		else:
+			print(roomContents[0])
+			
+		return True
+		
+	else:
+		return False
 
 def tableNode(action):
 	if "table" in action:
 		
-			if "sit" in action:
-				sitOnIt(table[0])
-				
-			elif "jump" in action:
-				jumpOnIt(table[0])
+		if "sit" in action:
+			sitOnIt(table[0])
 			
-			elif "under" in action:
-				duckAndCover(table[0])
+		elif "jump" in action:
+			jumpOnIt(table[0])
+		
+		elif "under" in action:
+			duckAndCover(table[0])
+		
+		elif "lift" in action:
+			lift(table[0])
+		
+		elif "search" in action:
+			search(table[0])
+		
+		elif(roomContents[1] == table[0]):
+			inspectObject(table)
+			print(roomContents[1])
+		
+		else:
+			print(roomContents[1])
 			
-			elif "lift" in action:
-				lift(table[0])
-			
-			elif "search" in action:
-				search(table[0])
-			
-			elif(roomContents[1] == table[0]):
-				inspectObject(table)
-				print(roomContents[1])
-			
-			else:
-				print(roomContents[1])
+		return True
+		
+	else:
+		return False
 				
 def bedNode(action):
 	if "bed" in action: 
 		
-			if "sit" in action:
-				sitOnIt(bed[0])
-				
-			elif "jump" in action:
-				jumpOnIt(bed[0])
+		if "sit" in action:
+			sitOnIt(bed[0])
 			
-			elif "under" in action:
-				duckAndCover(bed[0])
-			
-			elif "lift" in action:
-				lift(bed[0])
-			
-			elif "search" in action:
-				search(bed[0])
-			
-			elif(roomContents[2] == bed[0]):
-				inspectObject(bed)
-				print(roomContents[2])
-			
-			else:
-				print(roomContents[2])	
+		elif "jump" in action:
+			jumpOnIt(bed[0])
 		
+		elif "under" in action:
+			duckAndCover(bed[0])
+		
+		elif "lift" in action:
+			lift(bed[0])
+		
+		elif "search" in action:
+			search(bed[0])
+		
+		elif(roomContents[2] == bed[0]):
+			inspectObject(bed)
+			print(roomContents[2])
+		
+		else:
+			print(roomContents[2])	
+			
+		return True
+		
+	else:
+		return False
+
+def playerNode(action):
+	if "sit" in action:
+		print("You sit down cross-legged on the floor.")
+		
+	if "dance" in action:
+		print("You dance for a moment, though you are not sure why." 
+		+ "\nIt is almost as if you are a puppet whose strings are being"
+		+ "\npulled by the invisible hands of some unknown God..."
+		+ "\nYou quickly dismiss that thought and return to a standing position.")
+
+				
 #Game Loop
 def testLoop():
 	while(True):
@@ -185,9 +208,9 @@ def testLoop():
 		#UserAction(action)
 		
 		#do stuff with objects
-		chairNode(action)
-		tableNode(action)
-		bedNode(action)
+		if(chairNode(action) == False & tableNode(action) == False & bedNode(action) == False):
+			#Only access player node if you don't refer to any of the objects above
+			playerNode(action)
 		
 		#leave the game
 		if action == "exit":
