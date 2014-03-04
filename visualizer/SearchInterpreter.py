46447ba5ce1ebf150d9ff5cb0e3a8b85dcbc95bd
kyung01 <iopzxz@gmail.com>
2014-03-04 05:44:42 -0500

    fixed bugs
    names being displayed
    edges rendered
    Issues with alignment.
    Converse in class for further plan.

--- visualizer/SearchInterpreter.py
+++ visualizer/SearchInterpreter.py
@@ -9,49 +9,66 @@ import json
 # label : describe the node here 
 # { id,size, x,y, label} sweet
 # http://stackoverflow.com/questions/1323410/has-key-or-in
-_pos =0;
+_pos =-1;
 def convertToGraphNode(e):
 	global _pos
 	_pos += 1
 	return {'id' : e['id'], 
-			'size':3+ len(e['edges']),
-			'x':(_pos % 20 ) *3,'y': int( _pos / 20) *3 ,
-			'label': "Value : " + str(e['value']) + " Label : " + str(e['value'])}
+			'size': 1,
+			'x':(_pos % 20 ) ,'y': int( _pos / 20)  ,
+			'label': " Value [ " + str(e['value']) + " ] type [ " + str(e['type'])+" ]"}
+
+countEdge = 0;
+def convert2Edges(a,b):
+	global countEdge;
+	countEdge +=1;
+	return {'id' : str(countEdge), 'source':a['id'], 'target':b['id']}
 def convertEdgeToNode(e): return e['terminal']
 def helperAddNode(list,dic, e):
 	#if it's new element, add this to list of nodes and dictionary to remember 
 	if(not e['id'] in dic):
 		list.append(convertToGraphNode(e))
 		dic[e['id']] = list[len(list)-1]
-		print "AddNode " +str(e['id'])
-	else:
-		print "AddNode duplicate"
-def helperAddEdge(list,dic,edge):
+		#print "AddNode " +str(e['id'])
+	#else:
+		#print "AddNode duplicate"
+def helperAddEdge(list,dicNodes,dicEdges,edge):
 	small = edge[0]; big =edge[1]
 	if small['id'] > big['id']: small = edge[1]; big = edge[0]
 	
-	if not small['id'] in dic:
-		dic[small['id']] = [big['id']]
-		print "Edge New : "
+	#print dicNodes[edge[0]['id']]
+	for i in range(0,2):
+		dicNodes[edge[i]['id']]['size'] += 1 
+		if(int(dicNodes[edge[i]['id']]['size']) > 6 ):dicNodes[edge[i]['id']]['size']=6
+		
+	#print dicNodes[edge[0]['size']]
+	#list.append(convert2Edges(small,big) )
+	if not small['id'] in dicEdges:
+		list.append(convert2Edges(small,big) )
+		dicEdges[small['id']] = [big['id']]
+		#print "Edge New : "
 	#so this is not new edge to begin with, then is the connection recorded?
-	elif big['id'] in dic[small['id']] :
-		dic[small['id']].append(big['id'])
-		print "Edge added : "
-	else : print "Edge duplicate"
+	elif not big['id'] in dicEdges[small['id']] :
+		list.append(convert2Edges(small,big) )
+		dicEdges[small['id']].append(big['id'])
+		#print "Edge added : "
+	#else : print "Edge duplicate"
 	
 	return ""
 	
 def add(listNodes,listEdges, dicNodes,dicEdges, e):
-	print "Attempting to addNewNode...\nid    : " + str(e['id']) + "\nedges : " + str(len(e['edges']) )
+	#print "Attempting to addNewNode...\nid    : " + str(e['id']) + "\nedges : " + str(len(e['edges']) )
 	helperAddNode(listNodes,dicNodes,e);
 	edges = e['edges'] #recursively add edge nodes, hopefully no infinite returns 
 	for i in  range(0,len(edges) ):
 		newNode = edges[i]['terminal']
 		add(listNodes,listEdges,dicNodes,dicEdges,newNode);
-		helperAddEdge(listEdges,dicEdges,[e,newNode]);
+		helperAddEdge(listEdges,dicNodes,dicEdges,[e,newNode]);
 	#raw_input()
 	
 def read(s):
+	global _pos
+	_pos = -1
 	graphNodes = []
 	graphEdges = []
 	dicNodes = {} 
@@ -63,8 +80,7 @@ def read(s):
 		add(graphNodes,graphEdges,dicNodes,dicEdges,nodes[i])
 		
 		#print "AT " + str(i)
-	for i in range(0,5):
-		print graphNodes[i]
+
 		
 	#it = iter(nodes)
 	#while True:
@@ -72,4 +88,4 @@ def read(s):
 	#	raw_input()
 	#print item
 	#print "Interpreter loaded : " + nodes
-	return {'nodes':graphNodes , 'edges':[]}
+	return {'nodes':graphNodes , 'edges':graphEdges}
