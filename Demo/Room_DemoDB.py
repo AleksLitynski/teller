b8cc3aca122481f1749948dd4b9e9cb94e24c68d
Joe Prezioso <joeprezioso@comcast.net>
2014-03-15 15:52:27 -0400

    Had to put networkX into the folder to make things work. Tried to figure out more database stuff. I'll work on it more later.

--- Demo/Room_DemoDB.py
+++ Demo/Room_DemoDB.py
@@ -6,7 +6,9 @@ import socket
 #importing our stuff
 import json
 
-
+#must have foobar
+#can have arbitrary number of arguments and/or keyword arguments
+#def func(foobar, *args, **kwargs)
 
 #Nodes have: ID, type, value, & List of Edges
 #
@@ -24,7 +26,8 @@ import json
 def query(query_string):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('127.0.0.1', 5005))
-    s.send(query_string)
+    #print (type(s))                 #What is the type of s... -- It's a socket!
+    s.send(query_string)            #It doesn't seem to like this line...
     query_response = s.recv(10000) #our replies are VERY long. GOTTA fix that. At least, don't recurse into nodes that already exist
     s.close()
     return query_response
@@ -34,6 +37,12 @@ def query(query_string):
 def describe_noun(noun_name, depth=2):
 	#broke up the return into 2 lines to make it more readable
     return '{"type": "get", "params": {"depth":'+str(depth)+'}, "search": {"edges": [{"direction": "inbound","type": "describes","weight-time": "1","terminal": {"type": "relationship","edges": [{"terminal": {"type": "type","value": "named"}},{"terminal": {"type": "value","value": "'+noun_name+'"}}]}}]}}'
+
+#This will contain all the dictionaries returned from our JSON code
+encyclopedia = []
+
+#List of all Nouns
+nouns = {"id":[], "type":[], "value":[]}
 
 #want to create a list of nodes.
 nodes = {"info":[]}
@@ -143,15 +152,79 @@ def testLoop():
 #This code runs as soon as the game starts...	
 #Run the game!
 
+print("Creating Room...")
 #create room
 edgeInfo("contains", 1)
 nodeInfo(0, "noun", "A Room", "contains")
 
-#convert lots of json stuff into lists and dicts
-json.loads(query(describe_noun("room", 1)))
+#print(json.loads(query(describe_noun("room", 2))))
+print("\n")
+queryResult = json.loads(query(describe_noun("room", 2)))   #the 2 indicates we go down to a depth of 2
 
+if queryResult.get("type") == "get-success":
+    for response_node in queryResult.get("reply"):
+
+        #Idea: make this room a noun and then print it...
+        #This will require me to figure out how to use the noun class's print_noun method
+        
+        ##print response_node.get("id")
+        #print response_node.get("type")
+        #print response_node.get("value")
+
+        nouns["type"].append(response_node.get("type"))
+        #print (nouns["type"])
+        nouns["id"].append(response_node.get("id"))
+        #print (nouns["id"])
+        nouns["value"].append(response_node.get("value"))
+        #print (nouns["value"])
+
+        print("You are in a room.")
+
+        print("Inside, you see...")
+        
+        #Let's see if we can find/print the things a node is related to -- We can!
+        #print(response_node.get("edges"))
+        #Going off of that, let's try to get stuff from each node connected to the room
+        for edge_node in response_node.get("edges"):
+            print(edge_node.get("type"))            #what type of thing is this? -- describes
+            #if it is "describes", do stuff...
+            if edge_node.get("type") == "describes":
+                print("This describes something...")
+                #print(edge_node)    #just print everything in it...
+                #both print "none"
+                #print(edge_node.get("id"))
+                #print(edge_node.get("value"))
+            else:
+                print ("something odd...")
+                #check if any of these are nouns -- they are not
+                #print("not a noun")
+        
+        #queryresult = json.loads(query(describe_noun("room",2)))
+        """
+        if response_node.get("type") == "noun":
+            nouns["type"].append(response_node.get("type"))
+            print (nouns["type"])
+            nouns["id"].append(response_node.get("id"))
+            print (nouns["id"])
+            nouns["value"].append(response_node.get("value"))
+            print (nouns["value"])
+            """
+        
+
+"""
+#convert lots of json stuff into lists and dicts
+for d in json.loads(query(describe_noun("room", 1))):
+    #each dictionary is appended to the encyclopedia
+    print(type(d))
+    encyclopedia.append(d)  #not breaking things so far
+    print(encyclopedia[0])  #prints 'reply'...twice
+    print(encyclopedia[0][2])
 #look into query.reply...?
 #get element 0
+#for n in encyclopedia:
+"""
+
+    
 #take that element (has edges property, which corresponds to a list)
 #iterate over list -- each element of list has a type, which should be describes
 #find describes -- look at its terminal
