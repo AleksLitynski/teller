1f6383318a1784ea6d56b60914590dd53ecab79c
Aleks Litynski <ajl5242@rit.edu>
2014-04-22 10:28:05 -0400

    Added ajax code for running queires

--- tellerIDE/static/history/query-sender.js
+++ tellerIDE/static/history/query-sender.js
@@ -1,8 +1,24 @@
 state().loaded(function(){
 
 
+	document.querySelector(".run-query-button").onclick = function(){
+		send_query();
+	}
+
+
 	function send_query(query){
-		
+		jQuery.ajax({
+
+			type: "POST",
+			url: "query",
+			data: query,
+			success: receive_query,
+			dataType: "json"
+		})
+	}
+
+	function receive_query(data){
+		console.log(data);
 	}
 
 
